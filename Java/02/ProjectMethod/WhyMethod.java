4cebaf987386428d93c9dde3c10136d064a89054
InGyu Hwang <hwanginkyu42@naver.com>
2022-08-10 22:19:31 +0900

    java method

---
+++ Java/02/ProjectMethod/WhyMethod.java
@@ -0,0 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WhyMethod {
+
+	public static void main(String[] args) throws IOException {
+		String out = twoTimes("a", "-");
+		System.out.println(out);
+		FileWriter fw = new FileWriter("out.txt");
+		fw.write(out);
+		fw.close();
+		System.out.println(twoTimes("a", "*"));
+	}
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
+	}
+}
+//매개변수(parameter): 메소드에서 통용되는 변수
+//인자(argument): parameter에서 사용되는 변수
